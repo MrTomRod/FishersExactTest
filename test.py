19f2f15e81b6521c124edd144bc04f9d59a54c8c
MrTomRod <roder.thomas@gmail.com>
2022-01-18 11:53:35 +0100

    found disagreement between scipy and fast_fisher: fast_fisher was closer to real value

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import unittest
 from random import random
 from typing import Callable
 
+from math import factorial
+
 from scipy.stats import fisher_exact
 
 from fast_fisher import fast_fisher_python
@@ -14,18 +16,28 @@ except ImportError:
     from fast_fisher import fast_fisher_compiled
 
 
+def fisher_exact_factorial(a: int, b: int, c: int, d: int) -> float:
+    """
+    ((a+b)!*(c+d)!*(a+c)!*(b+d)!)
+    -----------------------------
+          (a!*b!*c!*d!*n!)
+    """
+    return (factorial(a + b) * factorial(c + d) * factorial(a + c) * factorial(b + d)) / \
+           (factorial(a) * factorial(b) * factorial(c) * factorial(d) * factorial(a + b + c + d))
+
+
 class TestFisher(unittest.TestCase):
-    def _test(self, function: Callable, name: str, args, scipy_l, scipy_r, scipy_t):
-        l, r, t = function(*args)
+    def test_with_scipy(self, samples=1000):
+        def _test(function: Callable, name: str, table):
+            l, r, t = function(*table)
 
-        for pval in (l, r, t):
-            self.assertLessEqual(pval, 1, f'Error in {name}: {pval=} must be smaller than 1! {args=}')
+            for pval in (l, r, t):
+                self.assertLessEqual(pval, 1, f'Error in {name}: {pval=} must be smaller than 1! {table=}')
 
-        self.assertAlmostEqual(l, scipy_l, msg=f'Error in {name}: {l} != {scipy_l}; {args=}')
-        self.assertAlmostEqual(r, scipy_r, msg=f'Error in {name}: {r} != {scipy_r}; {args=}')
-        self.assertAlmostEqual(t, scipy_t, msg=f'Error in {name}: {l} != {scipy_t}; {args=}')
+            self.assertAlmostEqual(l, scipy_l, msg=f'Error in {name}: {l=} != {scipy_l}; {table=}')
+            self.assertAlmostEqual(r, scipy_r, msg=f'Error in {name}: {r=} != {scipy_r}; {table=}')
+            self.assertAlmostEqual(t, scipy_t, msg=f'Error in {name}: {t=} != {scipy_t}; {table=}')
 
-    def test_with_scipy(self, samples=100):
         for _ in range(samples):
             a, b, c, d = [int(10 ** (random() * 4) - 1) for _ in range(4)]
 
@@ -33,9 +45,25 @@ class TestFisher(unittest.TestCase):
             scipy_r = fisher_exact([[a, b], [c, d]], 'greater')[1]
             scipy_t = fisher_exact([[a, b], [c, d]], 'two-sided')[1]
 
-            self._test(fast_fisher_python.test1, 'fast_fisher_python', (a, b, c, d), scipy_l, scipy_r, scipy_t)
-            self._test(fast_fisher_numba.test1, 'fast_fisher_numba', (a, b, c, d), scipy_l, scipy_r, scipy_t)
-            self._test(fast_fisher_compiled.test1, 'fast_fisher_compiled', (a, b, c, d), scipy_l, scipy_r, scipy_t)
+            _test(fast_fisher_python.test1, 'fast_fisher_python', (a, b, c, d))
+            _test(fast_fisher_numba.test1, 'fast_fisher_numba', (a, b, c, d))
+            _test(fast_fisher_compiled.test1, 'fast_fisher_compiled', (a, b, c, d))
+
+    def test_exception(self):
+        a, b, c, d = (2, 3, 0, 2)
+        scipy_t = fisher_exact([[a, b], [c, d]], 'two-sided')[1]
+        fast_t = fast_fisher_python.test1t(a, b, c, d)
+
+        # For this contingency table, scipy and this implementation do not agree!
+        self.assertNotAlmostEqual(fast_t, scipy_t)
+
+        factorial_t = fisher_exact_factorial(a, b, c, d)
+
+        scipy_error = abs(scipy_t - factorial_t)
+        fast_error = abs(fast_t - factorial_t)
+
+        # This implementation gives a better result than scipy's implementation!
+        self.assertTrue(fast_error < scipy_error)
 
 
 if __name__ == '__main__':
